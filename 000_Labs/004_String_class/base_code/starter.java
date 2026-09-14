843a6ea32344e68f1e2014c6c44428f7c38716d1
Tj5311 <Bigbrothatrucka@gmail.com>
2026-09-14 19:42:31 +0000

    update

--- 000_Labs/004_String_class/base_code/starter.java
+++ 000_Labs/004_String_class/base_code/starter.java
@@ -5,10 +5,81 @@
 
 import java.util.Scanner;
 
-class starter {
-	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
-	}
+class Starter {
+    public static void main(String[] args) {
+		System.out.println("Prayer by Kendrick Lamar");
+        System.out.println("I was born on February 24th, 1970");
+        System.out.println("The day I was delivered, they said I would build a legacy");
+        System.out.println("Picture me with influence that last generations");
+        System.out.println("I was made out of love to keep you happy and educated");
+        System.out.println("The first three letters of the alphabet my name");
+        System.out.println("I made you laugh, I made you dance, I made you sing");
+        System.out.println("I been around the world and back, you recorded my almanac");
+        System.out.println("So dearly way before the fact me and pops was estranged");
+        System.out.println("My father was a public figure, bigger than life");
+        System.out.println("Lived as a winner, on stages alike");
+        System.out.println("Rivers of people surely united, just to see our bond");
+        System.out.println("Time carried on, that was a common thread");
+        System.out.println("My perfect father had many flaws");
+        System.out.println("Still they moved they legs and shook they heads in unison");
+        System.out.println("He had more children, I was the oldest");
+        System.out.println("We always played with each other 'fore they ruined 'em");
+        System.out.println("He told me his personal secrets, childhood catching on beatings");
+        System.out.println("Sexually touched and abused, never seen life in a school");
+        System.out.println("The price of being iconic was demonic and screwed");
+        System.out.println("I stood on stage and had to watch my father on the news");
+        System.out.println("Stress on the head of my spirit, I was lost and confused");
+        System.out.println("I knew both sides of the story, but still, I couldn't choose");
+        System.out.println("See, it was more than humiliation and bein' sued");
+        System.out.println("They crucified my affiliation for points to prove");
+        System.out.println("And now my purpose for livin' makes no difference to you");
+        System.out.println("My total existence was for makin' a smiling move");
+        System.out.println("They said that I'll live forever");
+        System.out.println("But the moment he died, more accusations arrived");
+        System.out.println("I had to die too");
+        System.out.println("I remember takin' my first breath August 28th, 1963");
+        System.out.println("The whole nation had saved the day, I made it on TV");
+        System.out.println("My come out was a standing ova', the crowd in disbelief");
+        System.out.println("How could a group of words be so great?");
+        System.out.println("Designed to make you think");
+        System.out.println("Races and faces embracin' my conscience in letter form");
+        System.out.println("How gracious was I speakin' on changes once outside the norm?");
+        System.out.println("The person responsible, my creator");
+        System.out.println("He never conformed or used hatred to form mass retaliation");
+        System.out.println("Watch him perform with conviction");
+        System.out.println("Two hundred and fifty thousand souls left they homes to listen");
+        System.out.println("I gather his thoughts in abundance made to complete the mission");
+        System.out.println("His life became heroic, then tables turned");
+        System.out.println("My creator stripped from being a poet because he yearned");
+        System.out.println("Other women, infidelity stricken, I can't say he was perfect");
+        System.out.println("It made me nervous, they tear him down and forget me");
+        System.out.println("I prayed that I survived in the midst of the controversy");
+        System.out.println("How many lives'll fall on deaf ears if I'm not worthy?");
+        System.out.println("I won't justify his personal problems, that's not my job");
+        System.out.println("I was only born to make sure your children can play outside");
+        System.out.println("And I imagine the other demons he hid before he died");
+        System.out.println("If they came to surface, this dream probably won't be alive");
+        System.out.println("Because of you I was born in paranoia, it's poison for us");
+        System.out.println("We can't create with joyous ambition, we're out of luck");
+        System.out.println("The critics formed a new coalition and huddled up");
+        System.out.println("The non-creators are now elated and out of touch");
+        System.out.println("Why can't you gladly appreciate the influence we give?");
+        System.out.println("Stop starin' at me disgusted, my substance made sure you'd lived");
+        System.out.println("You want the head of the messenger and the history from him");
+        System.out.println("Which eventually takes away from the good you accustom");
+        System.out.println("Think of a life without the likes of imagination that's prosperin'");
+        System.out.println("The bodies that you consider genius, you like to box 'em in");
+        System.out.println("The great debaters and thinkers, they gave you a voice");
+        System.out.println("Why can't you prosecute the crimes and not the thoughts I employ?");
+        System.out.println("Because your better judgement will always look down on me");
+        System.out.println("But if I recollect how we connect, you'll account on me");
+        System.out.println("I got a theory that most of y'all won't allow to see");
+        System.out.println("It goes like this, talent doesn't choose morality");
+        System.out.println("See, if Daniel Hale was a killer, would you not want a heart?");
+        System.out.println("If Carl Benz was a racist, would you stop driving cars?");
+        System.out.println("I can't help we jump in these bodies and you called them a god");
+        System.out.println("Just know the Earth is just a rock without the voices of art");
+        System.out.println("Sincerely yours");
+    }
 }
+
--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -7,8 +7,31 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
+		String name;
+		int age;
+		int month;
+		int day;
+		int year;
+		double total;
+
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("what is your name?");
+		name = sc.nextLine();
+		System.out.println("how old are you?");
+		age = sc.nextInt();
+		System.out.println("what month were you born? (1-12)");
+		month = sc.nextInt();
+		System.out.println("what day were you born?");
+		day = sc.nextInt();
+		System.out.println("what year were you born?");
+		year = sc.nextInt();
+		System.out.println("if i have a dollar and two quarters, how much money do i have?");
+		total = sc.nextDouble();
+		System.out.println("your name is " + name + " you were born on " + month + "/" + day + "/" + year );
+		System.out.println("you are " + age + " years old!!!");
+		System.out.println("you have " + total + " dollars!!!");
+
 	}
 }
