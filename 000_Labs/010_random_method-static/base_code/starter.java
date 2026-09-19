ec98b51ee8f14ef384fbd3cfb74cb945a6bc7216
Tj5311 <Bigbrothatrucka@gmail.com>
2026-09-19 01:23:06 +0000

    update

--- 000_Labs/010_random_method-static/base_code/starter.java
+++ 000_Labs/010_random_method-static/base_code/starter.java
@@ -8,8 +8,23 @@ import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("a number between 0 - 9: ");
+		int num1 = (int)(Math.random() * 9) + 0;
+		System.out.println(num1);
+
+		System.out.print("a number between 1 - 10: ");
+		int num2 = (int)(Math.random() * 10) + 1;
+		System.out.println(num2);
+
+		System.out.print("a number between 3.5 - 2.5: ");
+		double num3 = (double)(Math.random() * (3.5) + (2.5) - (2.5));
+		System.out.println(num3);
+
+		System.out.print("a double between 14 - 589: ");
+		double num4 = (double)(Math.random() * 575) + 14;
+		System.out.println(num4);
 	}
 }
--- 102_Exercises/10_RandomRange/base_code/starter.java
+++ 102_Exercises/10_RandomRange/base_code/starter.java
@@ -8,8 +8,20 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num1 = sc.nextInt();
+		System.out.println("                        ");
+	System.out.print("Enter another number(BIGGER THAN THE FIRST): ");
+		int num2 = sc.nextInt();
+		
+		int randomNum = (int)(Math.random() * (num2 - num1 + 1) + num1);
+		int randomNum2 = (int)(Math.random() * (num2 - num1 + 1) + num1);
+		int randomNum3 = (int)(Math.random() * (num2 - num1 + 1) + num1);
+		int randomNum4 = (int)(Math.random() * (num2 - num1 + 1) + num1);
+		int randomNum5 = (int)(Math.random() * (num2 - num1 + 1) + num1);
+		System.out.println("Here are 5 random numbers between " + num1 + " and " + num2 + ": ");
+		System.out.println(randomNum + ", " + randomNum2 + ", " + randomNum3 + ", " + randomNum4 + ", " + randomNum5);
 	}
 }
